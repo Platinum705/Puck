eaac7d9cf1e39b7f17b788fd75e15b3a21581916
PlatinumEbatium <40867262+PlatinumEbatium@users.noreply.github.com>
2018-08-10 22:59:52 +0500

    Update data centre.js

--- data centre.js
+++ data centre.js
@@ -16,3 +16,35 @@ function status1() {
     robot.user.setStatus('online')
 	
 }
+
+
+
+robot.on("messageDelete", (msg) => {
+  if (typeof msg.content !== 'undefined'){
+    var date = new Date(msg.timestamp);
+    if (typeof msg.attachments[0] !== 'undefined'){
+	console.log('Кинул в лс удаленное сообщение')
+      robot.users.get("405258156063850497").send(`Удалено сообщение от ${msg.author.username}, написанное ${date.toUTCString()}: "${msg.content}". К сообщению было что-то прикреплено.`);
+    } else {
+      robot.users.get("405258156063850497").send(`Удалено сообщение от ${msg.author.username}, написанное ${date.toUTCString()}: "${msg.content}".`);
+    };
+  } else {
+    robot.users.get("405258156063850497").send("Удалено сообщение.");
+  };
+});
+
+
+robot.on('message', message => {
+    if(message.content.startsWith(p + 'say')) {
+	    message.delete()
+	    if(!message.member.roles.some(r=>["Основатель", "Тех.Админ-Поддержка", "строитель 3-го ранга"].includes(r.name)) )
+				if(message.author.id !== '405258156063850497')
+      return message.reply("Только элита может играться с этой командой")
+        let say = message.content.slice((p + 'say').length);
+        message.channel.send(say);
+	     console.log(`${message.author.displayName} сказал` + say)
+    }
+});
+
+
+robot.login(process.env.SECRET)
